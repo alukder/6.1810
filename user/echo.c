7419e0162b9c21516505c5ac399dd92769fa053c
alukder <2672847840@qq.com>
2024-04-06 03:16:49 -0400

    hello

--- user/echo.c
+++ user/echo.c
@@ -8,7 +8,9 @@ main(int argc, char *argv[])
   int i;
 
   for(i = 1; i < argc; i++){
-    write(1, argv[i], strlen(argv[i]));
+
+
+        write(1, argv[i], strlen(argv[i]));
     if(i + 1 < argc){
       write(1, " ", 1);
     } else {
